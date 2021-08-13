087961f88a714d06539580ebef5178fe9ea040ce
BartłomiejSocha <bart.soha1@gmail.com>
2021-08-13 10:23:09 +0200

    kts update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.5.21"
 }
 
 group = "org.example"
@@ -9,6 +12,32 @@ repositories {
     mavenCentral()
 }
 
+
+val junitVersion: String = "5.7.2"
+val compileKotlin: KotlinCompile by tasks
+val compileTestKotlin: KotlinCompile by tasks
+
+compileKotlin.kotlinOptions.suppressWarnings = true
+
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.21")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("com.natpryce:hamkrest:1.8.0.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+tasks {
+    withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "16"
+        }
+    }
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            showExceptions = true
+            exceptionFormat = TestExceptionFormat.FULL
+            events("passed", "skipped", "failed")
+        }
+    }
 }
