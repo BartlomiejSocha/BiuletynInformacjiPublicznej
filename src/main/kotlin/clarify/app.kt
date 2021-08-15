4e1e4980da383179074987604b77265cc3de189d
BartłomiejSocha <bart.soha1@gmail.com>
2021-08-15 17:54:02 +0200

    wip

--- src/main/kotlin/clarify/app.kt
+++ src/main/kotlin/clarify/app.kt
@@ -7,7 +7,6 @@ import org.http4k.server.Http4kServer
 import org.http4k.server.Jetty
 
 val app: HttpHandler = { Response(Status.OK) }
-
 fun server(port: Int): Http4kServer {
     return Jetty(port).toServer(app)
 }
